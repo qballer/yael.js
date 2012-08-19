b8a2ffaab0ef6c4352b72a764e590e34d25467f2
qballer <qballer@gmail.com>
2012-08-19 17:14:07 +0300

    removed unwanted test

--- test/div.js
+++
@@ -1,8 +0,0 @@
-var hello = function (){
-	var that = {};
-	that.sayHello = function (){
-		console.log("Hello World\n");
-	};
-	return that;
-};
-module.exports = hello;
--- test/experiment.js
+++
@@ -1,8 +0,0 @@
-var toType = function(obj){
-	return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
-};
-var path = "./div.js";
-var code = require (path);
-console.log(toType(code));
-var doit = new code();
-doit.sayHello();
